41866918f88f3f877dc1a6fbd48f56c0f582171d
JosephVolz <volzjoseph715@gmail.com>
2023-09-25 07:43:02 -0700

    Client: Add Components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArticleCreateComponent } from './components/article-create/article-create.component';
+import { ArticleUpdateComponent } from './components/article-update/article-update.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+import { ArticleItemComponent } from './components/article-item/article-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleCreateComponent,
+    ArticleUpdateComponent,
+    ArticleListComponent,
+    ArticleItemComponent
   ],
   imports: [
     BrowserModule
