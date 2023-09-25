186efc2d749a67206a2be6066fc13eed1fc2d8b9
JosephVolz <volzjoseph715@gmail.com>
2023-09-25 08:15:30 -0700

    Client: Add article service

---
+++ client/src/app/services/article.service..ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Article } from '../shared/article';
+import { config } from '../shared/config';
+
+const url = `${config.apiUrl}/article`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+
+  constructor(private readonly http: HttpClient) { }
+
+  getAll(): Observable<Article[]> {
+    return this.http
+      .get<Article[]>(`${url}`);
+  }
+
+  getById(id: string): Observable<Article> {
+    return this.http
+      .get<Article>(`${url}/${id}`);
+  }
+
+  create(article: Article): Observable<Article> {
+    return this.http
+      .post<Article>(`${url}`, article);
+  }
+
+  update(id: string, description: string): Observable<Article> {
+    return this.http
+      .put<Article>(`${url}/${id}`, {description});
+  }
+
+  delete(id: string): Observable<Article> {
+    return this.http
+      .delete<Article>(`${url}/${id}`);
+  }
+}
