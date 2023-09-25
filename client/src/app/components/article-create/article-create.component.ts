b1f002d1453988d6e75c78e08a4c3abb76d85aa3
JosephVolz <volzjoseph715@gmail.com>
2023-09-25 08:49:58 -0700

    Client: Add article create component

--- client/src/app/components/article-create/article-create.component.ts
+++ client/src/app/components/article-create/article-create.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Article } from '../../shared/types/article';
+import { ArticleService } from '../../services/article.service';
+import { ArticleValidator } from '../../shared/validators/articleValidator';
 
 @Component({
   selector: 'app-article-create',
@@ -6,5 +11,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./article-create.component.css']
 })
 export class ArticleCreateComponent {
+  articleForm = this.formBuilder.group({
+    title: new FormControl("", { nonNullable: true }),
+    content: new FormControl("", { nonNullable: true }),
+  });
+
+  constructor(private formBuilder: FormBuilder,
+            private articleService: ArticleService,
+            private router: Router){
+  }
+
+  onSubmit(): void {
+    const validator = new ArticleValidator();
+
+    const newArticle: Article = this.articleForm.value;
+    
+    const errors = validator.validate(newArticle);
+
+    console.log(errors);
+
+    if (Object.keys(errors).length === 0){
+      this.articleService.create(newArticle)
+      .subscribe({
+        next: () => this.router.navigateByUrl(''),
+        error: (err) => {
+          alert('Error');
+        },
+      });;
+    }
+    else{
+      alert('Input Correctly!');
+    }
+  }
 
 }
--- client/src/app/services/article.service.ts
+++ client/src/app/services/article.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { Article } from '../shared/article';
+import { Article } from '../shared/types/article';
 import { config } from '../shared/config';
 
 const url = `${config.apiUrl}/article`;
