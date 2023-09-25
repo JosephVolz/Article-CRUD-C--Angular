8b99ce0cdf9a37332097e387005c32554074148d
JosephVolz <volzjoseph715@gmail.com>
2023-09-25 11:37:20 +0100

    Server: Build basic structure

---
+++ server/DArticle/ApplicationLayer/Articles/ArticleService.cs
@@ -0,0 +1,9 @@
+using AutoMapper;
+using System;
+
+namespace DArticle.ApplicationLayer.Articles
+{
+    public class ArticleService : IArticleService
+    {      
+    }
+}
---
+++ server/DArticle/DomainModelLayer/Articles/Article.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+
+namespace DArticle.DomainModelLayer.Articles
+{
+    public class Article : IAggregateRoot
+    {
+    }
+}
