202ae48667f3d9bf81572f5e3623f2d64483a586
JosephVolz <volzjoseph715@gmail.com>
2023-09-25 07:01:29 -0700

    Server: Add Add and Remove in Article Repository

--- server/DArticle/InfrastructureLayer/MemArticleRepository.cs
+++ server/DArticle/InfrastructureLayer/MemArticleRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DArticle.Helpers.Domain;
+using DArticle.Helpers.Repository;
+using DArticle.DomainModelLayer.Articles;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +10,26 @@ namespace DArticle.InfrastructureLayer
 {
     public class ArticleRepository : IArticleRepository
     {
+        protected static List<Article> entities = new List<Article>();
+
+        public Article FindById(Guid id)
+        {
+            return entities.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public void Add(Article entity)
+        {
+            entities.Add(entity);
+        }
+
+        public void Remove(Article entity)
+        {
+            entities.Remove(entity);
+        }
+
+        public IEnumerable<Article> GetAll()
+        {
+            return entities;
+        }
     }
 }
