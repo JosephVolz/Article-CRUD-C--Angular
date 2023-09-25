5b85e8d2b191239bdaa09d3115caf48603daba19
JosephVolz <volzjoseph715@gmail.com>
2023-09-25 08:00:56 -0700

    Client: Init Article Read Component

---
+++ client/src/app/components/article-read/article-read.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-read',
+  templateUrl: './article-read.component.html',
+  styleUrls: ['./article-read.component.css']
+})
+export class ArticleReadComponent {
+
+}
