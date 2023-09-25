d4219a35dcb4ca4cf05822820e25126e99fca657
JosephVolz <volzjoseph715@gmail.com>
2023-09-25 14:08:43 +0100

    Server: Add Article Repository in Infrastructure Layer

--- server/DArticle/InfrastructureLayer/IArticleRepository.cs
+++ server/DArticle/InfrastructureLayer/IArticleRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DArticle.Helpers.Domain;
+using DArticle.DomainModelLayer.Articles;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +9,13 @@ namespace DArticle.Helpers.Repository
 {
     public interface IArticleRepository
     {
+        Article FindById(Guid id);
+
+
+        void Add(Article entity);
+
+        void Remove(Article entity);
+
+        IEnumerable<Article> GetAll();
     }
 }
